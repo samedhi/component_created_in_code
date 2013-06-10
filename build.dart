2b2f41f71df08bd8f67630f52b940617cf174a5e
Stephen Cagle <samedhi@gmail.com>
2013-06-10 14:09:30 -0700

    Initial Commit.

---
+++ build.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+import 'package:web_ui/component_build.dart';
+
+void main() {
+  build(new Options().arguments, ['web/component_created_in_code_test.html']);
+}
---
+++ web/component_created_in_code.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library component_created_in_code;
+
+import 'package:web_ui/web_ui.dart';
+
+class SayHello extends WebComponent {
+  String name;
+}
